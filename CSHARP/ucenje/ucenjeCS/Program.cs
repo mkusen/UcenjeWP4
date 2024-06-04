3043a9c3eacad5d0ee28a28bacdfecb562a39803
Mario Kušen <kusen.mario@gmail.com>
2024-06-04 18:15:15 +0200

    ucenjeCS second commit

--- CSHARP/ucenje/ucenjeCS/Program.cs
+++ CSHARP/ucenje/ucenjeCS/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * 
+ *komentar kroz više redova 
+ * 
+ * 
+ * 
+ */
+
+
 Console.WriteLine("Hello, World!");
+
+Console.Write("Mario");
+Console.Write(" Kušen");
+Console.WriteLine(value: " 2");
